90bf58a920fa8b093606eac4ec8869a25c8e04c5
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2020-07-10 22:46:03 -0400

    Add potential menu selection

--- client/main.go
+++ client/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	
+	//1. Total license count
+	//2. Search for host
+	//3. Contain host
+	//4. refresh token
 
 	//fmt.Println(api.AccessToken())
 	fmt.Println(api.FindHost("Mac"))
