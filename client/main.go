8c02316a61fee36eebec5decff1fa2f3911999da
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2020-07-23 23:58:00 -0400

    Add part of menu

--- client/main.go
+++ client/main.go
@@ -5,6 +5,29 @@ import (
 	"goStuff/CrowdStrikeAPI/api"
 )
 
+func menu() {
+	var choice int
+	for ok := true; ok; ok = (choice != 3) {
+		n, err := fmt.Scanln(&choice)
+		if n > 1 || err != nil {
+			fmt.Println("[!] Invalid input")
+			fmt.Println("[!] Entry not found, try again.")
+			continue
+		}
+		switch choice {
+		case 1:
+			fmt.Println("test1")
+		case 2:
+			fmt.Println(api.AccessToken())
+		case 3:
+			fmt.Println("Exiting Falcon...")
+			os.Exit(2)
+		}
+	}
+	return
+}
+
+
 func main() {
 	
 	//1. Total license count
