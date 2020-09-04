b793e71f90cd3e406be615489e1b8587e08f9001
Obehi Iredia <44153313+i0bj@users.noreply.github.com>
2020-09-03 20:41:29 -0400

    Add menu function

--- main.go
+++ main.go
@@ -39,6 +39,59 @@ func title() {
 	fmt.Println("Made with <3 by i_obj")
 }
 
+func menu() {
+	fmt.Println("1. Total Licenses Used")
+	fmt.Println("2. Get host metadata")
+	fmt.Println("3. Access Token")
+	fmt.Println("4. Find a Host")
+	fmt.Println("5. Start Batch Session")
+	fmt.Println("6. Run CMD")
+	fmt.Println("7. Exit")
+	// choice variable will hold the selection you make
+	// and will be used in the switch statement below
+	var choice int
+	for ok := true; ok; ok = (choice != 7) {
+		n, err := fmt.Scanln(&choice)
+		if n > 1 || err != nil {
+
+			continue
+		}
+		switch choice {
+		case 1:
+			for i := 0; i < 30; i++ {
+				time.Sleep(50 * time.Millisecond)
+				rtr.ProgressBar(i+1, 30, "Calculating", 10, "=")
+			}
+
+			api.LicenseTotal("5000")
+			var ret string
+			fmt.Println("\n\nReturn to main menu? y/n")
+			fmt.Scan(&ret)
+			if ret == "y" {
+				menu()
+			} else if ret == "n" {
+
+			}
+
+		case 2:
+			api.FindInfo(api.FindHost(""))
+		case 3:
+			fmt.Println("An Oauth2 token has been created.", )
+			
+		case 4:
+			fmt.Println(api.FindHost("))
+		case 5:
+			rtr.StartSession()
+		case 6:
+			rtr.ScriptRun("") //batch session ID
+		case 7:
+			fmt.Println("Exiting Falcon...")
+			os.Exit(0)
+		}
+	}
+
+}
+
 func programExit() {
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -55,52 +108,6 @@ func programExit() {
 func main() {
         programExit()
 	title()
+	menu()
 
-        fmt.Println("1. Total Licenses Used")
-	fmt.Println("2. Get host metadata")
-	fmt.Println("3. Access Token")
-	fmt.Println("4. Find a Host")
-	fmt.Println("5. Start Batch Session")
-	fmt.Println("6. Run CMD")
-	fmt.Println("7. Exit")
-	// choice variable will hold the selection you make
-	// and will be used in the switch statement below
-	var choice int
-	for ok := true; ok; ok = (choice != 3) {
-		n, err := fmt.Scanln(&choice)
-		if n > 1 || err != nil {
-			
-			continue
-		}
-		switch choice {
-			case 1:
-				for i := 0; i < 30; i++ {
-				time.Sleep(50 * time.Millisecond)
-				rtr.ProgressBar(i+1, 30, "Calculating", 10, "=")
-			}
-
-			api.LicenseTotal("5000")
-			var ret string
-			fmt.Println("\n\nReturn to main menu? y/n")
-			fmt.Scan(&ret)
-			if ret == "y" {
-				menu()
-			} else if ret == "n" {
-
-			}
-			api.LicenseTotal("5000")
-		case 2:
-			api.FindInfo(api.FindHost(""))
-		case 3:
-			fmt.Println(api.AccessToken())
-		case 4:
-			fmt.Println(api.FindHost(""))
-		case 5:
-			rtr.StartSession()
-		case 6:
-			rtr.ScriptRun("Batch session ID")
-		case 7:
-			fmt.Println("Exiting Falcon...")
-			os.Exit(0)
-		}
 	}
